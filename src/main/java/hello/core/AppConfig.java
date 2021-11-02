dd1edf383c91e191cf5e407577db9cf0ae64198b
blackjune67 <buea486@gmail.com>
2021-11-03 01:09:32 +0900

    간단히 스프링 적용, 코드 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,31 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration //설정정보
 public class AppConfig {
 
     //Construct Injection
+    @Bean //스프링컨테이너 등록!
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    public OrderService oderService() {
+    @Bean
+    public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy(); //할인정책을 바꿔본다.
@@ -45,7 +52,4 @@ public class AppConfig {
                 new FixDiscountPolicy());
     }
 */
-
-
-
 }
